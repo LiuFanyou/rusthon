079afd18b1b7c382ca58a20478669b43ceb192f7
Auceptin <wandererklein@icloud.com>
2026-03-26 21:43:18 +0800

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# Python 作为胶水语言调用 Rust 计算内核：基于 ctypes 与 PyO3 的实现、原理与性能比较
+
+## ctypes实现
+Todo
+
+## Pyo3实现
+Todo
+
---
+++ pyo3_lib/src/lib.rs
@@ -0,0 +1,10 @@
+/// 用rust写一个简单的逻辑回归
+/// 用pyo3封装成python库
+/// 用rust-numpy
+
+use pyo3::prelude::*;
+
+#[pyclass] // 用于把结构体处理成python类
+pub struct LogisticRegression{
+    
+}
